c8f808fb1e1ee13f2e872b7dff39d6c9327f7f9c
Diego Fourmentel <diego.fourmentel@gmail.com>
2021-06-23 06:55:46 -0300

    Filtro de la grilla

--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -10,15 +10,33 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   eventos: any = [];
+  eventosFiltrados: any = [];
+  _filtroLista : string = "";
+  get filtroLista(): string {
+    return this._filtroLista;
+  }
+  set filtroLista(value :string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getEventos();
   }
 
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter((evento: { tema: string; }) =>
+      evento.tema.toLocaleLowerCase().indexOf(filtrarPor) != -1
+    );
+  }
+
   getEventos() {
     this.http.get('https://localhost:5001/api/evento').subscribe(response => {
-      this.eventos = response
+      this.eventos = response;
+      this.eventosFiltrados = this.eventos;
     }, error => {
       console.log(error);
     });
